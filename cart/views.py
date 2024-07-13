2325a869daf472937bc3b47d0bee4f746824ad36
Kri-Bot01 <krishivjain8a@gmail.com>
2024-07-14 02:21:31 +0530

    "Updated cart functionality, added cart total calculation, and implemented message display for cart actions"

--- cart/views.py
+++ cart/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from core.models import Product
 from django.http import JsonResponse
-
+from django.contrib import messages
 # Create your views here.
 def cart_summary(request):
-	# Get the cart
-	cart = Cart(request)
-	cart_products = cart.get_prods
-	quantities = cart.get_quants
-	return render(request, "cart.html", {"cart_products":cart_products, "quantities":quantities})
-
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, "cart.html", {"cart_products":cart_products, "quantities":quantities,"totals":totals})
 
 def cart_add(request):
     cart = Cart(request)
@@ -24,7 +24,7 @@ def cart_add(request):
         
         #response = JsonResponse({'Product Name: ': product.name})
         response = JsonResponse({'qty': cart_quantity})
-        
+        messages.success(request, "Product Added To Cart...")
         return response
 
 
@@ -35,6 +35,7 @@ def cart_delete(request):
         
         cart.delete(product=product_id)
         response = JsonResponse({'product': product_id})
+        messages.success(request, "Product Removed From Cart...")
         return response
 
 def cart_update(request):
@@ -45,4 +46,5 @@ def cart_update(request):
 
         cart.update(product=product_id, quantity=product_qty)
         response = JsonResponse({'qty': product_qty})
+        messages.success(request, "Cart Updated")
         return response
