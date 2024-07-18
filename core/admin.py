9dbf4a764466306169daa95502812a29fb9f449a
Kri-Bot01 <krishivjain8a@gmail.com>
2024-07-19 00:39:09 +0530

    "Added Profile model and admin interface, extended User model, and updated admin registration for models"

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from .models import Product,Product_type,Product_size,Product_gender
-# Register your models here.
+from .models import Product, Product_type, Product_size, Product_gender, Profile
+from django.contrib.auth.models import User
 
+# Register your models here.
 admin.site.register(Product)
 admin.site.register(Product_type)
 admin.site.register(Product_gender)
 admin.site.register(Product_size)
+admin.site.register(Profile)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ("username", "first_name", "last_name", "email")
+    inlines = [ProfileInline]
+
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
